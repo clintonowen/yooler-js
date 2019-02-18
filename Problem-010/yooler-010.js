8520ac6c0adeaff4c22da47f44da066c69b806b2
Clinton Owen <clint@clintonowen.com>
2019-02-17 19:27:14 -0600

    Solve Problem 10

---
+++ Problem-010/yooler-010.js
@@ -0,0 +1,77 @@
+'use strict';
+
+/**
+ * ========== PROBLEM 10: Summation of primes ==========
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+
+/**
+ * ========== MY THOUGHTS ==========
+ * I already have a pretty good `isPrime` function from earlier problems, so
+ * I'll use that to iterate up to two million, keeping a running `sum` as I go.
+ */
+
+function InputException (message) {
+  this.message = message;
+  this.name = 'InputException';
+}
+
+function isPrime (n) {
+  // console.log(`Checking if ${n} is prime.`);
+  if (n < 2) {
+    return false;
+  }
+  let prime = true;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+}
+
+function sumPrimes (n) {
+  // Validate the input
+  if (!n || n < 1 || !Number.isInteger(n)) {
+    throw new InputException('Please enter a positive integer for `n`.');
+  }
+
+  if (n < 2) {
+    return `No primes found below ${n}.`;
+  } else if (n === 2) {
+    return 'The sum of all primes below 2 is 2.';
+  }
+
+  let sum = 2;
+
+  for (let i = 3; i < n; i += 2) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+
+  return `The sum of all primes below ${n} is ${sum}.`;
+}
+
+/* ========== INVALID INPUTS ========== */
+// console.log(sumPrimes());
+// console.log(sumPrimes(-10));
+// console.log(sumPrimes(10.33));
+// console.log(sumPrimes('10'));
+
+/* ========== VALID INPUTS ========== */
+// console.log(sumPrimes(1));
+// console.log(sumPrimes(2));
+// console.log(sumPrimes(100));
+// console.log(sumPrimes(1000));
+
+/* ========== TEST CASE ========== */
+// console.log(sumPrimes(10)); // => 17
+
+/* ========== SOLUTION ========== */
+console.log(sumPrimes(2000000)); // => 142913828922
+
+module.exports = sumPrimes;
--- Problem-010/yooler-010.test.js
+++ Problem-010/yooler-010.test.js
@@ -2,19 +2,19 @@
 'use strict';
 
 const chai = require('chai');
-const findPyTri = require('./yooler-009.js');
+const findPyTri = require('./yooler-010.js');
 const expect = chai.expect;
 
-describe('Problem 9', () => {
+describe('Problem 10', () => {
   describe('With a valid input:', () => {
     it('Returns the correct solution', () => {
-      const answer = findPyTri(12);
-      expect(answer).to.equal('The Pythagorean triplet which adds up to 12 is 3, 4, & 5 (whose product is 60).');
+      const answer = findPyTri(10);
+      expect(answer).to.equal('The sum of all primes below 10 is 17.');
     });
 
     it('Returns a message stating that no solution exists when applicable', () => {
       const answer = findPyTri(1);
-      expect(answer).to.equal('No Pythagorean triplet was found which adds up to 1.');
+      expect(answer).to.equal('No primes found below 1.');
     });
   });
 
